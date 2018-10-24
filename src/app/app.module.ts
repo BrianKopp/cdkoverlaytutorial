184ef4f7224e8de62afa1470163a9de817b29c45
Brian Kopp <briankopp.usa@gmail.com>
2018-10-24 07:31:51 -0600

    Add overlay component: ng g c overlay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverlayComponent
   ],
   imports: [
     BrowserModule
